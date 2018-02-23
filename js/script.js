bcd66f24b7709e183a0348da6c693678b5e5fa87
Pavlo Dasho <dashopasha@gmail.com>
2018-02-23 17:23:26 +0200

    pavlo

--- js/script.js
+++ js/script.js
@@ -1,16 +1,3 @@
 "use strict"
 
-var user = {
-  firstName: "Вася",
-  sayHi: function(who) { // здесь у sayHi есть один аргумент
-    alert( this.firstName + ": Привет, " + who );
-  }
-};
-
-var sayHi = bind(user.sayHi, user);
-
-sayHi("Петя"); // Вася: Привет, Петя
-sayHi("Маша"); // Вася: Привет, Маша
-
 document.write("branch Pavlo");
-document.write("branch master");
