8a121808224f8b4050f7fef820e220f7765b8399
Pavlo Dasho <dashopasha@gmail.com>
2018-03-02 19:36:15 +0200

    js Please enter the commit message for your changes. Lines starting

--- js/script.js
+++ js/script.js
@@ -1,7 +1,38 @@
 "use strict"
+ // ========================================5============================================
+/*Завдання 5: вам потрібно написати функцію яка приймає додатнє число і повертає наступне більше число, 
+утворене з тих самих цифер.
+
+Тести:
+
+biggerNumber(23) // 32;
+biggerNumber(624) // 642;
+biggerNumber(2018) // 2081;
+
+Якщо більшого числа немає, повернути -1
+
+biggerNumber(9) = -1;
+biggerNumber(111) = -1;
+biggerNumber(531) = -1;*/
  
- document.write("pavlo");
-document.write("branch master");
- document.write("pavlo");
-document.write("branch master");
-var na = "master";
+function biggerNumber (number) {
+
+	var strNumber = "" + number;
+	var arrNum = strNumber.split("");
+	var result = -1;
+
+	for (var i = 0; i < arrNum.length - 1; i++) {
+		if(arrNum[arrNum.length - i -1] > arrNum[arrNum.length - i - 2]){
+			arrNum.splice((arrNum.length - i -2), 2, arrNum[arrNum.length - i -1], arrNum[arrNum.length - i -2]);
+			result = +arrNum.join("");
+			return result;
+		} else{
+			continue;
+		}
+	}
+	return result;
+}
+
+
+document.write("Завдання 5."+ "<br>" + biggerNumber(531));
+// ==================================================================================
